46f1c7c8b31a78d382d89962eb07b03d9667e049
Max Kitsch <max.kitsch@gmail.com>
2014-10-11 14:58:50 +0300

    v0.0.1b - Roles added - Bugs fixed

--- index.js
+++ index.js
@@ -1,34 +1,70 @@
+var cookieParser = require('cookie-parser');
 var passport = require('passport');
 var session  = require('express-session');
+
+function isFunction(functionToCheck) {
+    var getType = {};
+    return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+}
 
 var BrestPassport =
 {
     init: function(brest){
-        brest.getApp().use(session({ secret: brest.getSetting('passport.secret','defaultpassportsecret')}));
+        brest.getApp().use(cookieParser());
+        var SessionStore = brest.getSetting('passport.store');
+        var sessionSettings = {
+            secret: brest.getSetting('passport.secret','defaultpassportsecret'),
+            cookie: { maxAge: 600000 }
+        };
+        if (isFunction(SessionStore)) sessionSettings.store = new SessionStore();
+        brest.getApp().use(session(sessionSettings));
         brest.getApp().use(passport.initialize());
         brest.getApp().use(passport.session());
-        brest.getPassport = function(){return passport};
     },
 
     method: {
-        authenticate: function(method, req, callback){
-            if (req.user) callback();
-            else callback(true);
-        },
 
-        beforeHandler: function(method, req, callback){
-            if (method.description.passportStrategy){
-                passport.authenticate(method.description.passportStrategy);
+        authenticate: function(method, req, callback){
+            if (req.user) {
+
+                if (req.user.roles && req.user.roles.indexOf('admin') > -1) {
+                    callback();
+                    return;
+                }
+
+                if (method.description.roles) {
+                    if (req.user.roles) {
+                            var roleChecked = false;
+                            for (var i=0; i<req.user.roles.length; i++){
+                                if (method.description.roles.indexOf(req.user.roles[i]) > -1) {
+                                    roleChecked = true;
+                                    break;
+                                }
+                            }
+                            if (!roleChecked) {
+                                callback({roles: 'failed'});
+                                return;
+                            }
+                        }
+                    }
+
+                if (method.description.denyRoles) {
+                    for (var i=0; i<req.user.roles.length; i++){
+                        if (method.description.denyRoles.indexOf(req.user.roles[i]) > -1) {
+                            callback({denyRoles: 'failed'});
+                            return;
+                        }
+                    }
+                }
+
+                callback();
             }
-            if (method.description.passportStrategyReturn) {
-                passport.authenticate(method.description.passportStrategyReturn,
-                    { successRedirect: brest.getSetting('passport.successRedirect','/'),
-                      failureRedirect: brest.getSetting('passport.failureRedirect','/login') });
-            }
-            if (method.description.passportLogout){
-                req.logout();
-            }
-            callback();
+            else callback(true);
         }
     }
 };
+
+BrestPassport.store = require('express-session/session/store');
+BrestPassport.passport = passport;
+
+module.exports = BrestPassport;
