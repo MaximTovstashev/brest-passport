2fd76e8f3f9a51357cbe95df986fffd960ba7c8c
Max Kitsch <max.kitsch@gmail.com>
2016-01-17 01:59:07 +0300

    v0.0.5 - Casting events

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var cookieParser = require('cookie-parser');
-var passport = require('passport');
-var session  = require('express-session');
+var _ = require('lodash'),
+    cookieParser = require('cookie-parser'),
+    passport = require('passport'),
+    session  = require('express-session');
 
 function isFunction(functionToCheck) {
     var getType = {};
@@ -24,47 +25,59 @@ var BrestPassport =
         brest.getApp().use(session(sessionSettings));
         brest.getApp().use(passport.initialize());
         brest.getApp().use(passport.session());
+        BrestPassport.brest = brest;
         callback();
     },
 
     method: {
 
+        /**
+         * Function is called after the express.js middleware and before the Brest handler.
+         * callback(true) stands for failed authentication.
+         * @param method
+         * @param req
+         * @param callback
+         */
         authenticate: function(method, req, callback){
-            if (req.user) {
+            console.log('incoming');
+            var i;
+            if (_.isEmpty(req.user)) {
+                BrestPassport.brest.emit('passport:fail', req.user);
+                return callback(true);
+            } else {
 
                 if (req.user.roles && req.user.roles.indexOf('admin') > -1) {
-                    callback();
-                    return;
+                    BrestPassport.brest.emit('passport:ok', req.user);
+                    return callback();
                 }
 
                 if (method.description.roles) {
                     if (req.user.roles) {
-                            var roleChecked = false;
-                            for (var i=0; i<req.user.roles.length; i++){
-                                if (method.description.roles.indexOf(req.user.roles[i]) > -1) {
-                                    roleChecked = true;
-                                    break;
-                                }
-                            }
-                            if (!roleChecked) {
-                                callback({roles: 'failed'});
-                                return;
+                        var roleChecked = false;
+                        for (i = 0; i < req.user.roles.length; i++) {
+                            if (method.description.roles.indexOf(req.user.roles[i]) > -1) {
+                                roleChecked = true;
+                                break;
                             }
                         }
-                    }
-
-                if (method.description.denyRoles) {
-                    for (var i=0; i<req.user.roles.length; i++){
-                        if (method.description.denyRoles.indexOf(req.user.roles[i]) > -1) {
-                            callback({denyRoles: 'failed'});
-                            return;
+                        if (!roleChecked) {
+                            BrestPassport.brest.emit('passport:fail', req.user);
+                            return callback({roles: 'failed'});
                         }
                     }
                 }
 
-                callback();
+                if (method.description.denyRoles) {
+                    for (i = 0; i < req.user.roles.length; i++) {
+                        if (method.description.denyRoles.indexOf(req.user.roles[i]) > -1) {
+                            BrestPassport.brest.emit('passport:fail', req.user);
+                            return callback({denyRoles: 'failed'});
+                        }
+                    }
+                }
+                BrestPassport.brest.emit('passport:ok', req.user);
+                return callback();
             }
-            else callback(true);
         }
     }
 };
