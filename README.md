7f6bf845ebf7fbfcdc1a694cd964db0bf78c0c67
Max Kitsch <max.kitsch@gmail.com>
2017-03-28 18:38:51 +0300

    v0.1.0 - Added documentation - Added adminRole - es6 refactoring

---
+++ README.md
@@ -0,0 +1,52 @@
+#Brest-passport
+
+[Passport.js](http://passportjs.org/) authentication wrapper for [Brest](https://github.com/MaximTovstashev/brest) library.
+
+##Installation
+
+In your project root run
+
+`$ npm install brest-passport --save`
+
+to install the latest version and save it to your `package.json` file.
+
+##Setup
+
+Upon Brest initialization, require `brest-passport` and add it to the plugins initialization inside `brest.ready` event callback.
+Use `BrestPassport.passport` to initialize authentication strategies.
+ 
+In this example we use passport local strategy initialization from passport.js official guide.
+
+```javascript
+const BrestPassport = require('brest-passport'),
+	  LocalStrategy = require('passport-local').Strategy;
+
+//...
+
+brest.on('ready', function(){
+
+	brest.use(
+	  [   BrestPassport ]);
+	
+	const passport = BrestPassport.passport;
+	
+	passport.use(new LocalStrategy(
+	  function(username, password, done) {
+		User.findOne({ username: username }, function(err, user) {
+		  if (err) { return done(err); }
+		  if (!user) {
+			return done(null, false, { message: 'Incorrect username.' });
+		  }
+		  if (!user.validPassword(password)) {
+			return done(null, false, { message: 'Incorrect password.' });
+		  }
+		  return done(null, user);
+		});
+	  }
+	));
+
+});
+```
+
+
+
--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-var _ = require('lodash'),
-    cookieParser = require('cookie-parser'),
-    passport = require('passport'),
-    session  = require('express-session'),
-    util = require('util');
+const _ = require('lodash'),
+  cookieParser = require('cookie-parser'),
+  passport = require('passport'),
+  session  = require('express-session');
+
+
+const EVENT_FAIL  = 'passport:fail',
+  EVENT_OK    = 'passport:ok';
 
 function isFunction(functionToCheck) {
-    var getType = {};
-    return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+  const getType = {};
+  return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
 }
 
-var BrestPassport =
-{
+const BrestPassport =
+  {
     init: function(brest, callback){
-        brest.getApp().use(cookieParser());
-        var sessionSettings = brest.getSetting('passport', {
-            secret: 'defaultpassportsecret',
-            cookie: { maxAge:  2629743830},
-            store: require('express-session/session/store')
-        });
-        if (isFunction(sessionSettings.store)) {
-            var SessionStore = sessionSettings.store(session);
-            var sessionStoreSettings = sessionSettings.storeSettingsKey ? brest.getSetting(sessionSettings.storeSettingsKey) : null;
-            sessionSettings.store = new SessionStore(sessionStoreSettings);
-        }
-        brest.getApp().use(session(sessionSettings));
-        brest.getApp().use(passport.initialize());
-        brest.getApp().use(passport.session());
-        BrestPassport.brest = brest;
-        callback();
+      brest.getApp().use(cookieParser());
+      const sessionSettings = brest.getSetting('passport', {
+        secret: 'defaultpassportsecret',
+        cookie: { maxAge:  2629743830},
+        store: require('express-session/session/store'),
+        adminRole: 'admin'
+      });
+      if (isFunction(sessionSettings.store)) {
+        const SessionStore = sessionSettings.store(session);
+        const sessionStoreSettings = sessionSettings.storeSettingsKey ? brest.getSetting(sessionSettings.storeSettingsKey) : null;
+        sessionSettings.store = new SessionStore(sessionStoreSettings);
+      }
+      brest.getApp().use(session(sessionSettings));
+      brest.getApp().use(passport.initialize());
+      brest.getApp().use(passport.session());
+      BrestPassport.brest = brest;
+      BrestPassport.adminRole = sessionSettings.adminRole;
+      callback();
     },
 
     method: {
@@ -39,50 +44,50 @@ var BrestPassport =
          * @param req
          * @param callback
          */
-        authenticate: function(method, req, callback){
-            var i;
-            if (_.isEmpty(req.user)) {
-                BrestPassport.brest.emit('passport:fail', req.user);
-                return callback({denied: "Authorisation failed"});
-            } else {
+      authenticate: function(method, req, callback){
+        let i;
+        if (_.isEmpty(req.user)) {
+          BrestPassport.brest.emit(EVENT_FAIL, req.user);
+          return callback({denied: 'Authorisation failed'});
+        } else {
 
-                if (req.user.roles && req.user.roles.indexOf('admin') > -1) {
-                    BrestPassport.brest.emit('passport:ok', req.user);
-                    return callback();
+          if (req.user.roles && req.user.roles.indexOf(BrestPassport.adminRole) > -1) {
+            BrestPassport.brest.emit(EVENT_OK, req.user);
+            return callback();
+          }
+
+          const methodFields = method.getFields();
+
+          if (methodFields.roles) {
+            if (req.user.roles) {
+              let roleChecked = false;
+              for (i = 0; i < req.user.roles.length; i++) {
+                if (methodFields.roles.indexOf(req.user.roles[i]) > -1) {
+                  roleChecked = true;
+                  break;
                 }
-
-                var methodFields = method.getFields();
-
-                if (methodFields.roles) {
-                    if (req.user.roles) {
-                        var roleChecked = false;
-                        for (i = 0; i < req.user.roles.length; i++) {
-                            if (methodFields.roles.indexOf(req.user.roles[i]) > -1) {
-                                roleChecked = true;
-                                break;
-                            }
-                        }
-                        if (!roleChecked) {
-                            BrestPassport.brest.emit('passport:fail', req.user);
-                            return callback({denied: "User has no role allowed for this method"});
-                        }
-                    }
-                }
-
-                if (methodFields.denyRoles) {
-                    for (i = 0; i < req.user.roles.length; i++) {
-                        if (methodFields.denyRoles.indexOf(req.user.roles[i]) > -1) {
-                            BrestPassport.brest.emit('passport:fail', req.user);
-                            return callback({denied: "User has some roles that are not allowed for this method"});
-                        }
-                    }
-                }
-                BrestPassport.brest.emit('passport:ok', req.user);
-                return callback();
+              }
+              if (!roleChecked) {
+                BrestPassport.brest.emit(EVENT_FAIL, req.user);
+                return callback({denied: 'User has no role allowed for this method'});
+              }
             }
+          }
+
+          if (methodFields.denyRoles) {
+            for (i = 0; i < req.user.roles.length; i++) {
+              if (methodFields.denyRoles.indexOf(req.user.roles[i]) > -1) {
+                BrestPassport.brest.emit(EVENT_FAIL, req.user);
+                return callback({denied: 'User has roles that are not allowed for this method'});
+              }
+            }
+          }
+          BrestPassport.brest.emit(EVENT_OK, req.user);
+          return callback();
         }
+      }
     }
-};
+  };
 
 BrestPassport.passport = passport;
 
